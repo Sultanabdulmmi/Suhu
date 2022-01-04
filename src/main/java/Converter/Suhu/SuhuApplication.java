785125df844e9f86ffc80f927f5ab6a9f9ac7e67
Sultanabdulmmi <96442245+Sultanabdulmmi@users.noreply.github.com>
2022-01-04 10:29:16 +0700

    Add files via upload

---
+++ src/main/java/Converter/Suhu/SuhuApplication.java
@@ -0,0 +1,13 @@
+package Converter.Suhu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuhuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuhuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Converter/Suhu/SuhuApplicationTests.java
@@ -0,0 +1,13 @@
+package Converter.Suhu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuhuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
